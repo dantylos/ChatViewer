ace0905d8829a25923f65f85732d7687018c7fb8
dantylos <danihme89@gmail.com>
2025-05-22 21:58:30 +0200

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.chat_viewer {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.chat_viewer to javafx.fxml;
+    exports com.example.chat_viewer;
+}
