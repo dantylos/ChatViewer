72d0fbef0ac54918496b60e3a6ed450bc455c3f6
dantylos <danihme89@gmail.com>
2025-05-23 13:22:21 +0200

    main chat configurations added; chat message representation;

--- src/main/java/com/chatViewer/MainApp.java
+++ src/main/java/com/chatViewer/MainApp.java
@@ -1,3 +1,27 @@
 package com.chatViewer;
 
+import com.chatViewer.controller.MainController;
+import com.chatViewer.view.MainView;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/* Main JavaFX application class that initializes and shows the chat viewer window. */
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        MainView view = new MainView();
+        new MainController(view);
+
+        Scene scene = new Scene(view, 800, 600);
+        primaryStage.setTitle("Chat Viewer");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /* Main method to launch the JavaFX application. @param args command-line arguments */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/chatViewer/view/MainView.java
@@ -0,0 +1,16 @@
+package com.chatViewer.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
